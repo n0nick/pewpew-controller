44c4990676c018be35cf0d3a4dca7de547c9880d
Sagie Maoz <sagie@maoz.info>
2013-04-24 12:52:41 +0300

    Trying stuff

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 App = Ember.Application.create();
 
+Server = DS.Model.extend({
+  ip_address: DS.attr('string'),
+
+  connect: function() {
+    console.log("supposed to connect to " + this.get('ip_address'));
+  }
+});
+
 App.Router.map(function() {
+  this.route("connect", { path: "/connect/:server" });
   this.route("build", { path: "/build" });
 });
 
 App.IndexRoute = Ember.Route.extend({
+  model: Server.createRecord()
+});
+
+App.ConnectRoute = Ember.Route.extend({
+  enter: function(p) {
+    console.log('bototm');
+  }
 });
 
 App.BuildRoute = Ember.Route.extend({
