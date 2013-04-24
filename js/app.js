e7435bf1c4da70cfdbb087a235e883c2d3bf1545
Sagie Maoz <sagie@maoz.info>
2013-04-24 14:16:13 +0300

    Logger, websocket connection

--- js/app.js
+++ js/app.js
@@ -1,19 +1,63 @@
 (function($, window){
   "use strict";
 
-  var Application = function() {
+  var Application = function(debug) {
     var app = this;
 
     var $pages = $('.page');
 
-    app.Pages = {
+    var log = function() {
+      if (app.debug && window.console) {
+        Function.prototype.apply.call(window.console.log, window.console,
+                                        Array.prototype.slice.call(arguments));
+      }
+    };
+
+    app.debug = false;
+
+    app.Connection = {
+      socket: null,
+
+      start: function(host) {
+        log("[Connection] Starting connection to", host);
+        this.socket = new window.WebSocket("ws://" + host);
+        this.socket.onopen = this.opened;
+        this.socket.onclose = this.closed;
+      },
+      opened: function() {
+      },
+      closed: function() {
+      }
+    },
+
+    app.Controllers = {
+      _app: app,
+
       _showOnly: function(id) {
         $pages.not(id).hide();
         return $pages.filter(id).show();
       },
 
+      _getParam: function(key) {
+        var query = window.location.search.substring(1),
+            vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+          var pair = vars[i].split('=');
+          if (decodeURIComponent(pair[0]) == key) {
+            return decodeURIComponent(pair[1]);
+          }
+        }
+      },
+
       _controllers: {
         index: function($page) {
+          var serverHost = this._getParam('server');
+          if (!serverHost) {
+            log("[Controller] No server address provided");
+            $('h2', $page).text('Please supply a server address.');
+          } else {
+            this._app.Connection.start(serverHost);
+          }
         },
 
         build: function($page) {
@@ -24,6 +68,8 @@
       },
 
       go: function(page) {
+        log("[Controller] viewing page", page);
+
         var $page = this._showOnly('#' + page);
         if (page in this._controllers) {
           this._controllers[page].call(this, $page);
@@ -32,7 +78,8 @@
     };
 
     app.start = function(){
-      app.Pages.go('index');
+      app.debug = !!(app.Controllers._getParam('debug'));
+      app.Controllers.go('index');
     };
 
   };
